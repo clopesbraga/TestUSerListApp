f4c7c108b34233eb82566cba441a061c0bcc1e89
clopesbraga <clopesbraga@gmail.com>
2025-08-11 16:34:00 -0300

    feature(TesterUserApp):Criando modulo para injeçao de dependencia

---
+++ app/src/main/java/br/com/clb/testuserlistapp/di/module/module.kt
@@ -0,0 +1,16 @@
+package br.com.clb.testuserlistapp
+
+import org.koin.android.ext.koin.androidApplication
+import org.koin.core.module.dsl.viewModel
+import org.koin.dsl.module
+
+val appModule = module {
+    single { UserRepository(androidApplication()) }
+}
+
+val viewModelModule = module {
+
+    viewModel { CreateUserViewModel(androidApplication(),get()) }
+    viewModel { ListUserViewModel(androidApplication(),get()) }
+
+}
